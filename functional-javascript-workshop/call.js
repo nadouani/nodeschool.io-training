4b9bdd714b338e5b5bcb38d833ff8a9e5e8d4ecd
Nabil ADOUANI <nabil.adouani@saint-gobain.com>
2015-02-03 10:02:43 +0100

    Finalize the use of Function#call example

--- functional-javascript-workshop/call.js
+++ functional-javascript-workshop/call.js
@@ -1,8 +1,8 @@
 function duckCount () {
-    var args = Array.prototype.slice.call(arguments, 1);
+    var args = Array.prototype.slice.call(arguments);
 
     return args.filter(function(item) {
-        return (item.hasOwnProperty && item.hasOwnProperty('quack')) || ("quack" in item);
+        return Object.prototype.hasOwnProperty.call(item, 'quack');
     }).length;
 
 }
